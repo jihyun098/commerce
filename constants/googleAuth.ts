cd3029d7f373a1a8a08a0accac5ea984481d9987
jihyun098 <hu3415@gmail.com>
2023-05-01 20:45:11 +0900

    google 로그인 연동

---
+++ constants/googleAuth.ts
@@ -0,0 +1,3 @@
+export const CLIENT_ID =
+  '55087938453-gdhivs94eshokieuau2f1rh3moedr70b.apps.googleusercontent.com'
+export const SECRET = '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'
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,20 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { GoogleOAuthProvider } from '@react-oauth/google'
+import { CLIENT_ID } from 'constants/googleAuth'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: { staleTime: Infinity },
+    },
+  })
+  return (
+    <GoogleOAuthProvider clientId={CLIENT_ID}>
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+      </QueryClientProvider>
+    </GoogleOAuthProvider>
+  )
 }
